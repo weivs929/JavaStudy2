33665135d1ffb7bf191737b9d76c7c922cc4f0a3
jijunwei <jijunwei@witenet.com>
2016-08-03 10:09:59 +0800

    080301

--- src/helloIdea.java
+++ src/helloIdea.java
@@ -1,10 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jjw on 2016/7/27.
  */
+@Data
 public class helloIdea {
+
+    private String ss;
+
+
     public static void main(String[] args) {
         String str = "hello word";
         System.out.printf("hello word!");
+        Map<String, Object> ss = new HashMap<String, Object>();
+
     }
 
 }
--- src/ideastu.java
+++ src/ideastu.java
@@ -1,11 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jjw on 2016/7/29.
  */
 public class ideastu {
+
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("添加dea  提交作者");
         System.out.println("1");
         System.out.println("2");
+        System.out.printf("");
+        System.out.println();
+        System.out.println("args = [" + args + "]");
     }
 }
